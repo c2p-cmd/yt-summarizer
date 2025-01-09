da4afdd2a9f3448b6d0afea8e5a4c13546fbd3fe
Sharan Thakur <sharandt19@live.com>
2025-01-09 19:29:42 +0100

    Add requirements.txt

---
+++ ios/YT-Summarizer-iOS/YT-Summarizer-iOS/YT_Summarizer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Summarizer_iOSApp.swift
+//  YT-Summarizer-iOS
+//
+//  Created by Sharan Thakur on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct YT_Summarizer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
